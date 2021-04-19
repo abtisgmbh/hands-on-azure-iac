49ead8d8c731ee5ec2dc8fa02f68c8e32d1eda69
Manuel Batsching <m.batsching@outlook.com>
2021-04-19 16:03:55 +0000

    feat: :sparkles: add tfenv configuration script

---
+++ Terraform/tfenv/configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
+
+

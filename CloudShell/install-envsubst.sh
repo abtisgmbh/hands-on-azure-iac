3ab3c62079b64aac4ff944e5caec9d80790b3e09
Manuel Batsching <m.batsching@outlook.com>
2021-04-28 09:40:12 +0000

    fix: :art: fix paths in install-envsubst script

--- CloudShell/install-envsubst.sh
+++ CloudShell/install-envsubst.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-wget https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.gz
-tar -xf gettext-0.21.tar.gz
-cd gettext-0.21/
+gettext=gettext-0.21
+
+wget https://ftp.gnu.org/pub/gnu/gettext/$gettext.tar.gz
+tar -xf $gettext.tar.gz
+cd $gettext/
 
 ./configure
 make
 make DESTDIR=$PWD install
  
-ln -s $PWD/usr/local/bin/envsubst ~/bin/envsubst
+ln -s $PWD/$gettext/usr/local/bin/envsubst ~/bin/envsubst
 

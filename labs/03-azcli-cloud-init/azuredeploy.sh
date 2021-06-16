4a735c152293d07c057b241bdcc57eef8e2cd3bf
Manuel Batsching <m.batsching@outlook.com>
2021-06-17 00:27:08 +0200

    feat: :sparkles: add lab for deploying docker host

--- labs/03-azcli-cloud-init/azuredeploy.sh
+++ labs/03-azcli-cloud-init/azuredeploy.sh
@@ -1,10 +1,12 @@
 rg=rg-docker-test
-vmName=docker-host
+vm_name=docker-host
+cloud_shell_ip=$(curl -s checkip.dyndns.org | sed -e 's/.*: //; s/<.*//')
 
+az group create -n $rg -l westeurope
 
-ip=$(az vm create \ 
+ip=$(az vm create \
 --resource-group $rg \
---name $vmName \
+--name $vm_name \
 --image UbuntuLTS \
 --admin-username azureuser \
 --generate-ssh-keys \
@@ -12,5 +14,19 @@ ip=$(az vm create \
 --query "[publicIps]" \
 --output tsv)
 
+# Allow the current cloud shell to access the docker host
+az network nsg rule create \
+--access Allow \
+--direction Inbound \
+--name allow-docker-cloud-shell-2375 \
+--nsg-name "${vm_name}NSG" \
+--priority 101 \
+--protocol Tcp \
+--resource-group $rg \
+--source-address-prefixes $cloud_shell_ip \
+--destination-port-ranges 2375
+
 export DOCKER_HOST=tcp://$ip:2375
 
+# Test if everything is setup correctly
+docker run --rm -it wernight/funbox cowsay "Hello CloudShell!"
